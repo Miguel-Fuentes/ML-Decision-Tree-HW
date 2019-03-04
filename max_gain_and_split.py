82ab86205bd03a8b54da1e0a02273151f548bef6
vgutta <vineethg@udel.edu>
2019-03-04 13:00:19 -0500

    max_gain and split

---
+++ max_gain_and_split.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[8]:
+
+def max_gain(self):
+    if val1:
+        if val1.max_gain(self)[1] > val0.max_gain(self)[1]:
+            return val1.max_gain(self)
+        if attributes:
+            return (this, max([heuristic(attribute) for attribute in attribute]))
+        else:
+            return (None, 0)
+
+
+# In[ ]:
+
+def split(self, attribute):
+    val0 = Node(atributes, remove(attribute), ancestors, append((attributes, data)))
+    val1 = Node(attributes, remove(attribute))
+
