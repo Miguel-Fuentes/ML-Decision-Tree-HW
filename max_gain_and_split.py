e47f49e6526ca5149441b0126bc191d9a522dd29
Miguel-Fuentes <Miguel.Fuentes.new@gmail.com>
2019-03-04 16:32:39 -0500

    Changes split to match constructor and use namedtuple

--- max_gain_and_split.py
+++ max_gain_and_split.py
@@ -7,15 +7,17 @@ def max_gain(self):
     if val1:
         if val1.max_gain(self)[1] > val0.max_gain(self)[1]:
             return val1.max_gain(self)
-        if attributes:
-            return (this, max([heuristic(attribute) for attribute in attribute]))
-        else:
-            return (None, 0)
+    if attributes:
+        return (this, max([heuristic(attribute) for attribute in attribute]))
+    else:
+        return (None, 0)
 
 
 # In[ ]:
 
 def split(self, attribute):
-    val0 = Node(atributes, remove(attribute), ancestors, append((attributes, data)))
-    val1 = Node(attributes, remove(attribute))
+    val0 = Node(self.atributes.remove(attribute), self.ancestors.append(attribute_value(attribute, 0)), 
+                self.data, self.heuristic)
+    val0 = Node(self.atributes.remove(attribute), self.ancestors.append(attribute_value(attribute, 1)), 
+                self.data, self.heuristic)
 
